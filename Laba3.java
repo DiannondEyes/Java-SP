29516cd245f59d22de01b2404131855c2934991c
MALWARE <53017160+ImMALWARE@users.noreply.github.com>
2023-09-04 09:48:38 +0000

    Вроде 3 задание теперь нормальное

--- Laba3.java
+++ Laba3.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Laba3 {
     public static void main(String[] args) {
-        List<Integer> numbers = new ArrayList<>();
+        List<Integer> numbers = new ArrayList<Integer>();
         int minMultiple37 = Integer.MAX_VALUE;
         int maxMultiple73 = Integer.MIN_VALUE;
         int pairCount = 0;
@@ -14,29 +14,21 @@ public class Laba3 {
 
         try (BufferedReader reader = new BufferedReader(new FileReader("17-342.txt"))) {
             String line;
-            int count = 0;
+            Integer number = null;
             while ((line = reader.readLine()) != null) {
-                if (count == 0) {
-                    int size = Integer.parseInt(line);
-                    numbers = new ArrayList<>(size);
-                } else {
-                    numbers.add(Integer.parseInt(line));
+                number = Integer.parseInt(line);
+                numbers.add(number);
+                if (number % 37 == 0 && number < minMultiple37) {
+                    minMultiple37 = number;
                 }
-                count++;
+                if (number % 73 == 0 && number > maxMultiple73) {
+                    maxMultiple73 = number;
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        for (int number : numbers) {
-            if (number % 37 == 0 && number < minMultiple37) {
-                minMultiple37 = number;
-            }
-            if (number % 73 == 0 && number > maxMultiple73) {
-                maxMultiple73 = number;
-            }
-        }
-
+        
         for (int i = 0; i < numbers.size() - 1; i++) {
             if ((numbers.get(i) == minMultiple37 && numbers.get(i+1) != maxMultiple73) || (numbers.get(i) != minMultiple37 && numbers.get(i+1) == maxMultiple73)) {
                 pairCount++;
