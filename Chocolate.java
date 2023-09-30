b79401bb255bd64dccc92f69184b29c791c3d9d3
ImMALWARE <notmalware@inbox.ru>
2023-09-30 17:58:56 +0300

    Chocolates

--- Chocolate.java
+++ Chocolate.java
@@ -7,10 +7,18 @@ public class Chocolate {
         int money = in.nextInt();
         System.out.print("Цена за шоколадку: ");
         int price = in.nextInt();
-        System.out.print("Количество обёрток: ");
+        System.out.print("Количество обёрток для бонусной шоколадки: ");
         int wrap = in.nextInt();
         in.close();
-        int wraps = money/price;
-        System.out.print("Максимум шоколадок: "+ ((wraps/wrap) + wraps));
+
+        int chocolates = money / price;
+        int wrappers = chocolates;
+
+        while (wrappers >= wrap) {
+            chocolates += (wrappers / wrap);
+            wrappers = wrappers % wrap + (wrappers / wrap);
+        }
+
+        System.out.println("Максимум шоколадок: " + chocolates);
     }
 }
